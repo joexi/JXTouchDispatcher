d40f4f75a990ac0f7d9a93a604a941a915ae88e0
JoeXi <407154812@qq.com>
2013-04-25 16:04:05 +0800

    Initial Commit

---
+++ TouchDispatcher/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  TouchDispatcher
+//
+//  Created by Joe on 13-4-25.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
